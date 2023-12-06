2b72b44c874dbcf0a89dc55b153133a32e5003b3
Cloy_101 <rockbit23@gmail.com>
2023-12-06 14:17:00 +0400

    init commit

---
+++ main.cpp
@@ -0,0 +1,22 @@
+﻿// reachEngine.cpp : Defines the entry point for the application.
+//
+
+#include "reachEngine.h"
+#include <iostream>
+#include <SDL.h>
+#include <glm.hpp>
+#include <tiny_obj_loader.h>
+#include <SDL_vulkan.h>
+#include <vulkan.h>
+
+
+
+int main()
+{
+	SDL_Event event;
+	
+	std::cout << "Hello CMake." << endl;
+	return 0;
+
+	
+}
